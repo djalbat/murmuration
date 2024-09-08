3f9ff20e0ae964e8b83b7f974b7a481326b4aefc
James Smith <james.smith@djalbat.com>
2024-09-08 18:15:49 +0100

    Cosmetic changes to the string utiliy functions.

--- bin/utilities/case.js
+++ bin/utilities/case.js
@@ -1,11 +1,20 @@
 "use strict";
 
 function camelCaseToSnakeCase(string) {
-  return string.replace(/([A-Z])/g, (match, character) => `_${character.toLowerCase()}`);
+  return string.replace(/([A-Z])/g, (match, character) => {
+    const lowerCaseCharacter = character.toLowerCase(),
+          characters = `_${lowerCaseCharacter}`
+
+    return characters;
+  });
 }
 
 function snakeCaseToCamelCase(string) {
-  return string.replace(/_(.)/g, (match, character) => character.toUpperCase());
+  return string.replace(/_(.)/g, (match, character) => {
+    const upperCaseCharacter = character.toUpperCase();
+
+    return upperCaseCharacter;
+  });
 }
 
 module.exports = {
