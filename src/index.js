fcabb84af31ffe3c6c73a622a1e19caebaa2f93c
James Smith <james.smith@djalbat.com>
2025-10-29 20:53:20 +0000

    Propagated.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-export { default as migrate } from "./src/migrate";
-export { default as database } from "./src/database";
-export { default as Statement } from "./src/statement";
-export { default as defaultLog } from "./src/defaultLog";
-export { default as transaction } from "./src/transaction";
-export { default as caseUtilities } from "./src/utilities/case";
-export { default as CustomMigration } from "./src/migrate/customMigration";
+export { default as migrate } from "./migrate";
+export { default as database } from "./database";
+export { default as Statement } from "./statement";
+export { default as defaultLog } from "./defaultLog";
+export { default as transaction } from "./transaction";
+export { default as caseUtilities } from "./utilities/case";
+export { default as CustomMigration } from "./migrate/customMigration";
